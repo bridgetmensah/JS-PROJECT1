4c71792a0499b12435820e38728ad42bdb8e3b4b
Bridget-Momo <bridgetmomo123@gmail.com>
2023-06-15 17:10:26 +0000

    a simple welcome page

---
+++ js/welcome.js
@@ -0,0 +1,9 @@
+let myName = "Bridget Mensah "
+let greeting ="Hi dear "+ myName + ", welcome back continue your courses! 😀"
+let welcomeMessage= greeting 
+ document.getElementById("welcome-el").innerText=welcomeMessage
+ let confirm ="Great! Hit OK to continue😀"
+  function welcome(){
+    alert(confirm)
+
+  }
