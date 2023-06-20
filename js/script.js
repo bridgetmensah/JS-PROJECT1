91d5ac89b24fa4316e6e4bc8bf1b4d2e81ad7be0
Bridget-Momo <bridgetmomo123@gmail.com>
2023-06-20 16:01:06 +0000

    removed unecessary comments

--- js/script.js
+++ js/script.js
@@ -1,34 +1,4 @@
-// document.getElementById("count-el").innerText=5
-// let count = 0
-// count +=1
-// console.log(count)
-// let myAge=12
-// console.log(myAge)
-// let humanDogRatio=25
-// let myDogAge= myAge * humanDogRatio
-// console.log(myDogAge)
 
-// let bonusPoint = 50
-// console.log(bonusPoint)
-// bonusPoint +=50
-// console.log(bonusPoint)
-// bonusPoint-=75
-// console.log(bonusPoint)
-// bonusPoint+=45
-// console.log(bonusPoint)
-
-// function add(){
-//   console.log(2+4)
-// }
-// add() 
-
-// let lap1 = 34
-// let lap2 = 33
-// let lap3 = 36
-// function sum(){
-//     console.log(lap1 +lap2+lap3)
-// } 
-// sum()
 let count =0
 
 function increment() {
